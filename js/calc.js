da1ebb31a97c215b199f573fe55696b1e5f4ded9
Rabbitinhat <yaoyao99471@hotmail.com>
2019-06-16 23:55:58 +0900

    1.4

---
+++ js/calc.js
@@ -0,0 +1,11 @@
+var a = +prompt("a: ");
+var b = +prompt("b: ");
+var c = +prompt("c: ");
+var d = +prompt("d: ");
+var e = +prompt("e: ");
+var f = +prompt("f: ");
+var y = (a * f - c * d) / (a * e - d * b);
+var x = (b * f - c * e) / (b * d - a * e);
+
+console.log(x, y);
+
